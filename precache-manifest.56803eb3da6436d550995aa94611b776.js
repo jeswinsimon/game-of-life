355a3c149d8e487c4c7e82e8f6864954affbed75
jeswinsimon <jeswinsimon@gmail.com>
2020-04-18 21:18:34 +0530

    Updates

---
+++ precache-manifest.56803eb3da6436d550995aa94611b776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcf3b5fa1f1b5ad522917f5f877c662",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "dd567cea2622d2fd85d1",
+    "url": "/game-of-life/static/css/main.f5e82217.chunk.css"
+  },
+  {
+    "revision": "c80f3d6d982cd4968f47",
+    "url": "/game-of-life/static/js/2.a8864106.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/game-of-life/static/js/2.a8864106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd567cea2622d2fd85d1",
+    "url": "/game-of-life/static/js/main.86751166.chunk.js"
+  },
+  {
+    "revision": "39f8af036aee07664d58",
+    "url": "/game-of-life/static/js/runtime-main.1c12863f.js"
+  }
+]);
